b7a1f5417ca4505f4e977dbaea7a23db3d09bd5e
marco gobbi <marcogobbi0@gmail.com>
2022-02-24 09:35:56 +0100

    #2 free and destroy yoga nodes

--- packages/layout/src/layout.js
+++ packages/layout/src/layout.js
@@ -3,15 +3,29 @@ import { createStyles } from './create/create-styles';
 import { createYogaNodes } from './create/create-yoga-nodes';
 import * as R from 'ramda';
 
-function triggerLayoutCalculation(node) {
+const triggerLayoutCalculation=(node)=> {
   node._yogaNode?.calculateLayout();
   return node;
 }
+
+const freeYogaNodes = node => {
+  node._yogaNode?.freeRecursive();
+  return node;
+};
+
+const destroyYogaNodes = node => {
+  return R.compose(
+    R.dissoc('_yogaNode'),
+    R.evolve({ children: R.map(destroyYogaNodes) }),
+  )(node);
+};
 
 export function getLayout(ctx, doc) {
 
   return R.evolve({
     children: R.map(R.compose(
+      destroyYogaNodes,
+      freeYogaNodes,
       calculateLayout,
       triggerLayoutCalculation,
       createYogaNodes(ctx),
