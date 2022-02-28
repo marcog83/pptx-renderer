1242d3eda0056dcf189f483431a18ffd0ca26d32
marco gobbi <marcogobbi0@gmail.com>
2022-02-28 14:21:47 +0100

    #2 cleanup

--- packages/layout/src/assets/fetchImage.js
+++ packages/layout/src/assets/fetchImage.js
@@ -1,24 +1,22 @@
 import { sizeOf } from '@pptx-renderer/image-size';
 import { Buffer } from 'buffer';
+
+function calculateImage(buffer){
+
+    const size = sizeOf(buffer); 
+    console.log(size);
+    const encoding='base64'; 
+    var uri = 'data:' + size.type + ';' + encoding + ',' + buffer.toString(encoding); 
+    return {
+        ...size,
+        data:uri
+    }
+}
+
 async function _fetchImage(url) {
     return fetch(url)
-        .then(async (response) => {
-            const arrayBuffer = await response.arrayBuffer();
-            return Buffer.from(arrayBuffer);
-        })
-        .then((buffer) => {
-
-          
-            const size = sizeOf(buffer); 
-            console.log(size);
-            const encoding='base64'; 
-            var uri = 'data:' + size.type + ';' + encoding + ',' + buffer.toString(encoding); 
-            return {
-                ...size,
-                data:uri
-            }
-            
-        });
+        .then(async (response) => Buffer.from(await response.arrayBuffer()))
+        .then(calculateImage);
 }
 
 
